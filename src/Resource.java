4ea68b056b8aa9b0382a9d920344862d03386296
Gervasio Santos <gervasio.neto94@gmail.com>
2015-05-31 21:58:24 +1000

    comments added

--- src/Resource.java
+++ src/Resource.java
@@ -5,10 +5,14 @@ Gervasio Protasio dos Santos Neto - 5050769
 Ruan De Menezes Costa - 5050761
 */
 
+/*
+  Class to represent Gold, Dynamites and Axe
+*/
+
 public class Resource{
 
 	private Position resourcePosition;
-
+    
 	protected Resource(int r, int c) {
 		resourcePosition = new Position(r,c);
 	}
--- src/StateComparator.java
+++ src/StateComparator.java
@@ -10,7 +10,9 @@ import java.util.Comparator;
 
 
 public class StateComparator implements Comparator<MyState> {
-
+    /*Compares two states given their F values. This is necessary for
+     the Priority Queue used by A* to know which one has highest
+     priority.*/
 	@Override
 	public int compare(MyState o1, MyState o2) {
 		return o1.getFValue() - o2.getFValue();
